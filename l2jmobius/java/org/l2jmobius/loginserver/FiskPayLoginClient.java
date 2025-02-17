587885323abf7f6683c94a0a63d751f8fa759ccd
emmastat <94614837+FiskPay@users.noreply.github.com>
2025-02-17 14:27:24 +0200

    logWithdraw does not need to be async

--- l2jmobius/java/org/l2jmobius/loginserver/FiskPayLoginClient.java
+++ l2jmobius/java/org/l2jmobius/loginserver/FiskPayLoginClient.java
@@ -56,26 +56,24 @@ public class FiskPayLoginClient implements Listener
     @Override
     public void onLogWithdraw(String txHash, String to, String symbol, String amount, String server, String character, String refund)
     {
-        LSProcessor.logWithdraw(txHash, to, symbol, amount, server, character, refund).thenAccept((success)->{
-            
-            String nowDate = getDateTime();
-            
-            if (success)
-            {
-                BLOCKCHAIN_LOGGER.info(nowDate + " | Withdrawal on " + getServerName(server) + ": " + character + " -> " + to + " = " + amount + " " + symbol);
-            }
-            else
-            {
-                BLOCKCHAIN_LOGGER.warning(nowDate + " | -------------------------------------- Failed Withdrawal Start -------------------------------------");
-                BLOCKCHAIN_LOGGER.warning(nowDate + " | TxHash: " + txHash);
-                BLOCKCHAIN_LOGGER.warning(nowDate + " | From:   " + character);
-                BLOCKCHAIN_LOGGER.warning(nowDate + " | To:     " + to);
-                BLOCKCHAIN_LOGGER.warning(nowDate + " | Server: " + getServerName(server));
-                BLOCKCHAIN_LOGGER.warning(nowDate + " | Amount: " + amount);
-                BLOCKCHAIN_LOGGER.warning(nowDate + " | Token:  " + symbol);
-                BLOCKCHAIN_LOGGER.warning(nowDate + " | --------------------------------------- Failed Withdrawal End --------------------------------------");
-            }
-        });
+        String nowDate = getDateTime();
+        boolean success = LSProcessor.logWithdraw(txHash, to, symbol, amount, server, character, refund);
+         
+        if (success)
+        {
+            BLOCKCHAIN_LOGGER.info(nowDate + " | Withdrawal on " + getServerName(server) + ": " + character + " -> " + to + " = " + amount + " " + symbol);
+        }
+        else
+        {
+            BLOCKCHAIN_LOGGER.warning(nowDate + " | -------------------------------------- Failed Withdrawal Start -------------------------------------");
+            BLOCKCHAIN_LOGGER.warning(nowDate + " | TxHash: " + txHash);
+            BLOCKCHAIN_LOGGER.warning(nowDate + " | From:   " + character);
+            BLOCKCHAIN_LOGGER.warning(nowDate + " | To:     " + to);
+            BLOCKCHAIN_LOGGER.warning(nowDate + " | Server: " + getServerName(server));
+            BLOCKCHAIN_LOGGER.warning(nowDate + " | Amount: " + amount);
+            BLOCKCHAIN_LOGGER.warning(nowDate + " | Token:  " + symbol);
+            BLOCKCHAIN_LOGGER.warning(nowDate + " | --------------------------------------- Failed Withdrawal End --------------------------------------");
+        }
     }
     
     @Override
