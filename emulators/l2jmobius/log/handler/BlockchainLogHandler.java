cd8049356d3e969cf694670ef84d8b9e4e96e7f5
Scrabakos <scrab4ever@hotmail.com>
2025-04-12 14:41:08 +0300

    Uploaded BlockchainLogHandler

---
+++ emulators/l2jmobius/log/handler/BlockchainLogHandler.java
@@ -0,0 +1,13 @@
+// Game Server: BlockchainLogHandler.java
+package org.l2jmobius.log.handler;
+
+import java.io.IOException;
+import java.util.logging.FileHandler;
+
+public class BlockchainLogHandler extends FileHandler
+{
+	public BlockchainLogHandler() throws IOException
+	{
+		super();
+	}
+}
